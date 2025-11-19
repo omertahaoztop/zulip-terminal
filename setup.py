05d233bcdfe33e4c702d57d4ddc8a256c0b341b0
Ömer Taha Öztop <taha.oztop@mono.tr>
2025-11-20 00:07:08 +0300

    Add Python 3.12 and 3.13 support

--- setup.py
+++ setup.py
@@ -18,21 +18,21 @@ def long_description():
 
 
 testing_minimal_deps = [
-    "pytest>=7.4.0,<8.0.0",  # 8.0.0 drops support for Python 3.7
-    "pytest-mock>=3.10.0,<3.12.0",  # 3.12.0 drops support for Python 3.7
+    "pytest>=7.4.0",
+    "pytest-mock>=3.10.0",
 ]
 
 testing_plugin_deps = [
-    "pytest-cov>=4.0.0,<5.0.0",  # 5.0.0 drops support for Python 3.7 (6.0.0 drops 3.8)
+    "pytest-cov>=4.0.0",
 ]
 
 testing_deps = testing_minimal_deps + testing_plugin_deps
 
 linting_deps = [
-    "isort~=5.11.0,<5.12.0",  # 5.12.0 drops support for Python 3.7, 6.0.0 drops 3.8
-    "black==23.3.0",  # Later versions drop support for Python 3.7
-    "ruff==0.0.267",
-    "codespell[toml]==2.2.5,<2.2.6",  # 2.2.6 drops support for Python 3.7
+    "isort>=5.11.0",
+    "black>=23.3.0",
+    "ruff>=0.0.267",
+    "codespell[toml]>=2.2.5",
     "typos>=1.32.0",
 ]
 
@@ -42,7 +42,7 @@ gitlint_deps = [
 
 typing_deps = [
     "lxml-stubs",
-    "mypy~=1.8.0",  # >=1.9.0 requires Python 3.8+, >=1.15.0 requires 3.9+
+    "mypy>=1.8.0",
     "types-beautifulsoup4",
     "types-pygments",
     "types-python-dateutil",
@@ -76,6 +76,8 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
     ],
@@ -85,7 +87,7 @@ setup(
         "Issues": "https://github.com/zulip/zulip-terminal/issues",
         "Hot Keys": "https://github.com/zulip/zulip-terminal/blob/main/docs/hotkeys.md",
     },
-    python_requires=">=3.7, <3.12",
+    python_requires=">=3.7, <3.14",
     keywords="",
     packages=find_packages(exclude=["tests", "tests.*"]),
     zip_safe=True,
@@ -106,15 +108,15 @@ setup(
     tests_require=testing_deps,
     install_requires=[
         "urwid~=2.1.2",
-        "zulip>=0.8.2,<0.9.0",  # Next release, 0.9.0, requires Python 3.9
+        "zulip>=0.8.2",
         "urwid_readline>=0.15.1",
         "beautifulsoup4>=4.13.4",
-        "lxml==4.9.4",
-        "pygments>=2.17.2,<2.18.0",  # 2.18.0 will drop support for Python 3.7
-        "typing_extensions~=4.5.0",
+        "lxml>=4.9.4",
+        "pygments>=2.17.2",
+        "typing_extensions>=4.5.0",
         "python-dateutil>=2.8.2",
-        "pytz>=2025",  # Can use native support from Python 3.9 onwards
-        "tzlocal>=5.0,<5.1",  # 5.1 will drop support for Python 3.7
+        "pytz>=2025",
+        "tzlocal>=5.0",
         "pyperclip>=1.9.0",
     ],
 )
